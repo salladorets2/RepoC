dac46aac510d52a2d6b01ae795cb5a4109376364
Tsar09 <tsar0911@gmail.com>
2017-03-16 09:51:26 +0200

    refactoring

--- prgTchLB1/Program.cs
+++ prgTchLB1/Program.cs
@@ -1,4 +1,4 @@
-п»їusing System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +9,12 @@ namespace prgTchLB1
     static class Program
     {
         /// <summary>
-        /// Р“Р»Р°РІРЅР°СЏ С‚РѕС‡РєР° РІС…РѕРґР° РґР»СЏ РїСЂРёР»РѕР¶РµРЅРёСЏ.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
+		 //Application starts
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
